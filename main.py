27e160e18d542d43265d8c3e4c0aeb0f1b93f18f
Dušan Lazić <lazicdusan@protonmail.com>
2024-02-19 21:31:35 +0100

    Add xgboost

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import train_test_split, cross_val_score, GridSearc
 from sklearn.metrics import mean_absolute_error
 from sklearn.linear_model import ElasticNet
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from scipy import stats
 
 
@@ -20,19 +21,43 @@ class DataLoader:
 
     def remove_outliers(self, data):
         data = data[data["godina proizvodnje"] >= 2002]
-        data = data.drop(data[(data['godina proizvodnje'] <= 2005) & (data['cena'] > 6000)].index)
-        data = data.drop(data[(data['godina proizvodnje'] <= 2007) & (data['cena'] > 11000)].index)
-        data = data.drop(data[(data['godina proizvodnje'] <= 2010) & (data['cena'] > 15000)].index)
-        data = data.drop(data[(data['godina proizvodnje'] >= 2010) & (data['cena'] < 4000)].index)
-        data = data.drop(data[(data['godina proizvodnje'] >= 2015) & (data['cena'] < 7000)].index)
-        data = data.drop(data[(data['godina proizvodnje'] >= 2019) & (data['cena'] < 12000)].index)
-        data = data.drop(data[(data['snaga motora'] >= 200) & (data['cena'] < 3000)].index)
-        data = data.drop(data[(data['snaga motora'] >= 250) & (data['cena'] < 3500)].index)
-        data = data.drop(data[(data['snaga motora'] >= 300) & (data['cena'] < 6500)].index)
-        data = data.drop(data[(data['snaga motora'] >= 400) & (data['cena'] < 12000)].index)
+        data = data.drop(
+            data[(data["godina proizvodnje"] <= 2005) & (data["cena"] > 6000)].index
+        )
+        data = data.drop(
+            data[(data["godina proizvodnje"] <= 2007) & (data["cena"] > 11000)].index
+        )
+        data = data.drop(
+            data[(data["godina proizvodnje"] <= 2010) & (data["cena"] > 15000)].index
+        )
+        data = data.drop(
+            data[(data["godina proizvodnje"] >= 2010) & (data["cena"] < 4000)].index
+        )
+        data = data.drop(
+            data[(data["godina proizvodnje"] >= 2015) & (data["cena"] < 7000)].index
+        )
+        data = data.drop(
+            data[(data["godina proizvodnje"] >= 2019) & (data["cena"] < 12000)].index
+        )
+        data = data.drop(
+            data[(data["snaga motora"] >= 200) & (data["cena"] < 3000)].index
+        )
+        data = data.drop(
+            data[(data["snaga motora"] >= 250) & (data["cena"] < 3500)].index
+        )
+        data = data.drop(
+            data[(data["snaga motora"] >= 300) & (data["cena"] < 6500)].index
+        )
+        data = data.drop(
+            data[(data["snaga motora"] >= 400) & (data["cena"] < 12000)].index
+        )
         data = data[data["kilometraža"] < 375000]
-        data = data.drop(data[(data['kilometraža'] > 350000) & (data['cena'] > 25000)].index)
-        data = data.drop(data[(data['kilometraža'] > 300000) & (data['cena'] > 35000)].index)
+        data = data.drop(
+            data[(data["kilometraža"] > 350000) & (data["cena"] > 25000)].index
+        )
+        data = data.drop(
+            data[(data["kilometraža"] > 300000) & (data["cena"] > 35000)].index
+        )
         data = data[np.abs(stats.zscore(data["cena"])) <= 3]
         return data
 
@@ -56,9 +81,9 @@ class DataLoader:
             encoded_transmission = pd.get_dummies(
                 data["menjač"], prefix="menjač"
             ).reindex(columns=self.columns_transmission, fill_value=0)
-            encoded_ac = pd.get_dummies(
-                data["klima"], prefix="klima"
-            ).reindex(columns=self.columns_ac, fill_value=0)
+            encoded_ac = pd.get_dummies(data["klima"], prefix="klima").reindex(
+                columns=self.columns_ac, fill_value=0
+            )
         else:
             encoded_make = pd.get_dummies(data["marka"], prefix="marka")
             encoded_body = pd.get_dummies(data["karoserija"], prefix="karoserija")
@@ -101,7 +126,9 @@ class DataLoader:
 
 
 def cross_validate(X_train, Y_train, regressor):
-    scores = cross_val_score(regressor, X_train, Y_train, cv=5, scoring='neg_mean_absolute_error')
+    scores = cross_val_score(
+        regressor, X_train, Y_train, cv=5, scoring="neg_mean_absolute_error"
+    )
     predicted_values = np.exp(-scores)
 
     print("ElasticNet Cross-Validation MAE Scores:", predicted_values)
@@ -110,11 +137,16 @@ def cross_validate(X_train, Y_train, regressor):
 
 def get_elastic_net_best_estimator(X_train, Y_train):
     elastic_net_param_grid = {
-        'alpha': [0.0005, 0.001, 0.002, 0.005, 0.0075, 0.01, 0.025, 0.05],
-        'l1_ratio': [0.05, 0.1, 0.15, 0.2, 0.25, 0.35, 0.4, 0.5, 0.6, 0.8]
+        "alpha": [0.0005, 0.001, 0.002, 0.005, 0.0075, 0.01, 0.025, 0.05],
+        "l1_ratio": [0.05, 0.1, 0.15, 0.2, 0.25, 0.35, 0.4, 0.5, 0.6, 0.8],
     }
     elastic_net_regressor = ElasticNet(random_state=0)
-    elastic_net_grid_search = GridSearchCV(elastic_net_regressor, elastic_net_param_grid, cv=5, scoring='neg_mean_absolute_error')
+    elastic_net_grid_search = GridSearchCV(
+        elastic_net_regressor,
+        elastic_net_param_grid,
+        cv=5,
+        scoring="neg_mean_absolute_error",
+    )
     elastic_net_grid_search.fit(X_train, Y_train)
     print("Best parameters for ElasticNet:", elastic_net_grid_search.best_params_)
     return elastic_net_grid_search
@@ -122,13 +154,19 @@ def get_elastic_net_best_estimator(X_train, Y_train):
 
 def get_random_forest_best_estimator(X_train, Y_train):
     random_forest_param_grid = {
-        'n_estimators': [225, 250, 275],
-        'max_depth': [None],
-        'min_samples_split': [5, 10],
-        'min_samples_leaf': [1],
+        "n_estimators": [225, 250, 275],
+        "max_depth": [None],
+        "min_samples_split": [5, 10],
+        "min_samples_leaf": [1],
     }
     random_forest_regressor = RandomForestRegressor(random_state=0)
-    random_forest_grid_search = GridSearchCV(random_forest_regressor, random_forest_param_grid, cv=5, scoring='neg_mean_absolute_error', verbose=1)
+    random_forest_grid_search = GridSearchCV(
+        random_forest_regressor,
+        random_forest_param_grid,
+        cv=5,
+        scoring="neg_mean_absolute_error",
+        verbose=1,
+    )
     random_forest_grid_search.fit(X_train, Y_train)
     print("Best parameters for Random Forest:", random_forest_grid_search.best_params_)
 
@@ -154,6 +192,31 @@ def test(regressor, X_val, Y_val, label):
     print(f"MAE for {label}: {mae}")
 
 
+def get_xgboost_best_estimator(X_train, Y_train):
+    xgboost_param_grid = {
+        "n_estimators": [100, 200, 300, 1000],
+        "learning_rate": [0.01, 0.05, 0.1, 0.15],
+        "max_depth": [3, 4, 5, 6, 7],
+        "subsample": [0.8, 0.9, 1.0],
+    }
+
+    # Best parameters for XGBoost: {'learning_rate': 0.1, 'max_depth': 5, 'n_estimators': 1000, 'subsample': 0.9}
+    # MAE for XGBoost regressor: 1693.20137255967
+
+    xgboost_regressor = XGBRegressor(random_state=0, verbosity=1)
+    xgboost_grid_search = GridSearchCV(
+        xgboost_regressor,
+        xgboost_param_grid,
+        cv=5,
+        scoring="neg_mean_absolute_error",
+        verbose=2,
+    )
+    xgboost_grid_search.fit(X_train, Y_train)
+    print("Best parameters for XGBoost:", xgboost_grid_search.best_params_)
+
+    return xgboost_grid_search
+
+
 def main():
     data_loader = DataLoader("vehicles/vehicles.csv")
     data = data_loader.load_data()
@@ -171,13 +234,15 @@ def main():
     # elastic_net_regressor = elastic_net_grid_search.best_estimator_
     # elastic_net_regressor = ElasticNet(alpha=0.0005, l1_ratio=0.05, random_state=0)
 
-    random_forest_grid_search = get_random_forest_best_estimator(X_train, Y_train)
-    random_forest_regressor = random_forest_grid_search.best_estimator_
+    # random_forest_grid_search = get_random_forest_best_estimator(X_train, Y_train)
+    # random_forest_regressor = random_forest_grid_search.best_estimator_
     # random_forest_regressor = RandomForestRegressor(random_state=0)
-
 
     # cross_validate(X_train, Y_train, elastic_net_regressor)
     # cross_validate(X_train, Y_train, random_forest_regressor)
+
+    xgboost_grid_search = get_xgboost_best_estimator(X_train, Y_train)
+    xgboost_regressor = xgboost_grid_search.best_estimator_
 
     test_data = data_loader.preprocess(test_data, False)
     x_test = test_data.drop("cena", axis=1)
@@ -185,7 +250,8 @@ def main():
 
     x_test = prep_data_for_test(data_loader, x_test)
     # test(elastic_net_regressor, x_test, y_test, label='Elastic net regressor')
-    test(random_forest_regressor, x_test, y_test, label='Random forest regressor')
+    # test(random_forest_regressor, x_test, y_test, label="Random forest regressor")
+    test(xgboost_regressor, x_test, y_test, label="XGBoost regressor")
 
 
 if __name__ == "__main__":
